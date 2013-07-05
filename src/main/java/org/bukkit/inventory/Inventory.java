d716c1124fe9900015abfcc7259100620570324b
Aikar <aikar@aikar.co>
2013-07-04 21:04:26 -0400

    Inventory Meta API

--- src/main/java/org/bukkit/inventory/Inventory.java
+++ src/main/java/org/bukkit/inventory/Inventory.java
@@ -14,6 +14,7 @@ import org.bukkit.event.inventory.InventoryType;
  */
 public interface Inventory extends Iterable<ItemStack> {
 
+	public HashMap<String, Object> getMeta(); // EMC
     /**
      * Returns the size of the inventory
      *
